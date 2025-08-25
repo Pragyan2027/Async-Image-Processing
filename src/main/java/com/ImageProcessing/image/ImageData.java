472792146718764761c852c3bdcb19ea12494ac3
Pragyan Sen <146320930+Pragyan2027@users.noreply.github.com>
2025-08-26 00:40:19 +0530

    Update ImageData.java

--- src/main/java/com/ImageProcessing/image/ImageData.java
+++ src/main/java/com/ImageProcessing/image/ImageData.java
@@ -1,4 +1,35 @@
 package com.ImageProcessing.image;
 
+import java.awt.image.BufferedImage;
+
 public class ImageData {
+    private final BufferedImage image;
+    private int i;  // position X
+    private int j;  // position Y
+    private int width;
+    private int height;
+
+    public ImageData(BufferedImage image, int i, int j, int width, int height) {
+        this.image = image;
+        this.i = i;
+        this.j = j;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getI() {
+        return i;
+    }
+    public int getJ() {
+        return j;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public BufferedImage getImage() {
+        return image;
+    }
 }
