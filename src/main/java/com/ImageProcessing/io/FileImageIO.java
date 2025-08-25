e9ad2fbf6899e5c9c984686279d7647312cb1d53
Pragyan Sen <146320930+Pragyan2027@users.noreply.github.com>
2025-08-26 00:40:39 +0530

    Update FileImageIO.java

--- src/main/java/com/ImageProcessing/io/FileImageIO.java
+++ src/main/java/com/ImageProcessing/io/FileImageIO.java
@@ -1,4 +1,25 @@
 package com.ImageProcessing.io;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
-public class FileImageIO {
+public class FileImageIO implements ImageReadInf {
+
+    @Override
+    public <T> BufferedImage readImage(T src){
+        try{
+            String path = (String) src;
+            File imageFile = new File(path);
+            return ImageIO.read(imageFile);
+        }catch (Exception ex){
+            System.err.println("Not able to read the image");
+            return null;
+        }
+    }
+
+    @Override
+    public void sendImage(BufferedImage src){
+        // Todo: implement this function to store the imageFile
+    }
+
 }
