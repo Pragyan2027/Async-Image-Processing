1e274e8a8e296a675b550454a2f9b12327059fd6
Pragyan Sen <146320930+Pragyan2027@users.noreply.github.com>
2025-08-26 00:41:50 +0530

    Update ImageProcessor.java

--- src/main/java/com/ImageProcessing/processor/ImageProcessor.java
+++ src/main/java/com/ImageProcessing/processor/ImageProcessor.java
@@ -1,4 +1,30 @@
-package com.chat.processor;
+package com.ImageProcessing.processor;
+
+import com.ImageProcessing.filter.ImageFilter;
+import com.ImageProcessing.image.ImageData;
+
+
+import java.awt.image.BufferedImage;
 
 public class ImageProcessor {
+
+    public void processImage(BufferedImage image, int tileSize,
+                             ImageFilter filter,
+                             DrawMultipleImagesOnCanvas canvas) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        for (int y = 0; y < height; y += tileSize) {
+            for (int x = 0; x < width; x += tileSize) {
+                int w = Math.min(tileSize, width - x);
+                int h = Math.min(tileSize, height - y);
+
+                BufferedImage tile = image.getSubimage(x, y, w, h);
+                BufferedImage processed = filter.apply(tile);
+
+                canvas.addImageToQueue(new ImageData(processed, x, y, w, h));
+            }
+        }
+    }
 }
+
